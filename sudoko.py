e0c9a8c87543ffecd43d4685023a7a2b5843df3d
Pratyush Chaudhary <pratyushc232@gmail.com>
2020-12-05 01:49:09 +0530

    Put mat as class member.

--- sudoko.py
+++ sudoko.py
@@ -3,70 +3,83 @@ from math import sqrt
 
 class SudokuSolver():
 
-    def __init__(self, N):
+    def preprocess_mat(self, mat):
+        if isinstance(mat, str):
+            lst = mat.split()
+            mat = [[int(x) for x in lst[i*9:i*9+9]] for i in range(0,9)]
+            return mat
+        elif isinstance(mat, list):
+            return mat
+        else:
+            raise ValueError("Sudoku can be inputted only as string or list of lists.")
+
+    def __init__(self, N, mat):
         self.N = N # no of rows, no of cols, no of digits
         self.SQ_N = int(sqrt(N))
         self.VALID_SET = set(range(1,N+1))
+        self.mat = self.preprocess_mat(mat)
 
-    def print_sudoku(self, mat):
+    def print_sudoku(self):
         for i in range(self.N):
             if i%self.SQ_N == 0:
                 print("-"*38)
             for j in range(self.N):
                 if j%self.SQ_N == 0:
                     print('|', end='  ')
-                print(mat[i][j], end='  ')
+                print(self.mat[i][j], end='  ')
             print('|')
         print("-"*38)
 
-    def is_valid_square(self, mat, start_row, start_col):
+    def is_valid_square(self, start_row, start_col):
         current_set = set()
         for i in range(start_row, start_row+self.SQ_N):
             for j in range(start_col, start_col+self.SQ_N):
-                current_set.add(mat[i][j])
+                current_set.add(self.mat[i][j])
         return current_set == self.VALID_SET
 
-    def check_squares(self, mat):
+    def check_squares(self):
         is_valid_conf = True
         start_row = 0
         for i in range(self.SQ_N):
             start_col = 0
             for j in range(self.SQ_N):
-                is_valid_conf &= self.is_valid_square(mat, start_row, start_col)
+                is_valid_conf &= self.is_valid_square(start_row, start_col)
                 start_col += self.SQ_N
             start_row += self.SQ_N
         return is_valid_conf
 
-    def is_valid_row(self, mat, row_no):
+    def is_valid_row(self, row_no):
         current_set = set()
         for i in range(self.N):
-            current_set.add(mat[row_no][i])
+            current_set.add(self.mat[row_no][i])
         return current_set == self.VALID_SET
 
-    def check_rows(self, mat):
+    def check_rows(self):
         valid_rows = True
         for i in range(self.N):
-            valid_rows&= self.is_valid_row(mat, i)
+            valid_rows&= self.is_valid_row(i)
         return valid_rows
     
-    def is_valid_col(self, mat, col_no):
+    def is_valid_col(self, col_no):
         current_set = set()
         for i in range(self.N):
-            current_set.add(mat[i][col_no])
+            current_set.add(self.mat[i][col_no])
         return current_set == self.VALID_SET
     
-    def check_cols(self, mat):
+    def check_cols(self):
         valid_cols = True
         for i in range(self.N):
-            valid_cols &= self.is_valid_col(mat, i)
+            valid_cols &= self.is_valid_col(i)
         return valid_cols
     
-    def is_valid_configuration(self, mat):
+    def is_valid_configuration(self):
         is_valid_conf = True
-        is_valid_conf &= self.check_squares(mat)
-        is_valid_conf &= self.check_rows(mat)
-        is_valid_conf &= self.check_cols(mat)
+        is_valid_conf &= self.check_squares()
+        is_valid_conf &= self.check_rows()
+        is_valid_conf &= self.check_cols()
         return is_valid_conf
     
+
+
     def solve_sudoku(self, mat):
-        return self.is_valid_configuration(mat)
+        return self.is_valid_configuration()
--- test_sudoku.py
+++ test_sudoku.py
@@ -1,29 +1,31 @@
 from unittest import TestCase
 from unittest import main as unit_main
 from sudoko import SudokuSolver
-  
+
+def get_sample_mat_valid():
+    sudoku =    """
+                7	2	6	4	9	3	8	1	5
+                3	1	5	7	2	8	9	4	6
+                4	8	9	6	5	1	2	3	7
+                8	5	2	1	4	7	6	9	3
+                6	7	3	9	8	5	1	2	4
+                9	4	1	3	6	2	7	5	8
+                1	9	4	8	3	6	5	7	2
+                5	6	7	2	1	4	3	8	9
+                2	3	8	5	7	9	4	6	1
+                """
+    lst = sudoku.split()
+    mat = [[int(x) for x in lst[i*9:i*9+9]] for i in range(0,9)]
+    return mat
+
 class SudokuTest(TestCase): 
 
-    sudoku = SudokuSolver(9)
+    sudoku = SudokuSolver(9, get_sample_mat_valid())
 
     def rotate(self, l, n):
         return l[n:] + l[:n]
 
-    def get_sample_mat_valid(self):
-        sudoku =    """
-                    7	2	6	4	9	3	8	1	5
-                    3	1	5	7	2	8	9	4	6
-                    4	8	9	6	5	1	2	3	7
-                    8	5	2	1	4	7	6	9	3
-                    6	7	3	9	8	5	1	2	4
-                    9	4	1	3	6	2	7	5	8
-                    1	9	4	8	3	6	5	7	2
-                    5	6	7	2	1	4	3	8	9
-                    2	3	8	5	7	9	4	6	1
-                    """
-        lst = sudoku.split()
-        mat = [[int(x) for x in lst[i*9:i*9+9]] for i in range(0,9)]
-        return mat
+   
     
     def get_sample_mat_invalid(self):
         sample_mat = []
@@ -33,20 +35,16 @@ class SudokuTest(TestCase):
         return sample_mat
     
     def test_valid_rows(self):
-        mat = self.get_sample_mat_valid()
-        self.assertTrue(self.sudoku.check_rows(mat))
+        self.assertTrue(self.sudoku.check_rows())
     
     def test_valid_cols(self):
-        mat = self.get_sample_mat_valid()
-        self.assertTrue(self.sudoku.check_cols(mat))
+        self.assertTrue(self.sudoku.check_cols())
     
     def test_valid_squares(self):
-        mat = self.get_sample_mat_valid()
-        self.assertTrue(self.sudoku.check_squares(mat))
+        self.assertTrue(self.sudoku.check_squares())
     
     def test_valid_conf(self):
-        mat = self.get_sample_mat_valid()
-        self.assertTrue(self.sudoku.is_valid_configuration(mat))
+        self.assertTrue(self.sudoku.is_valid_configuration())
 
 if __name__ == '__main__': 
     unit_main() 
